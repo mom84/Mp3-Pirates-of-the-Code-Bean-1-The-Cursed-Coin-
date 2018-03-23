953c0d6601510358f7a32065dfa74c2fd85b7134
unknown <engmohammmad1984@yahoo.com>
2018-03-23 18:11:33 +0100

    Fixed circle ci

--- src/test/java/at/refugeescode/Mp3Pirates/of/the/Code/Bean1/The/Cursed/Coin/JackSparrowTest.java
+++ src/test/java/at/refugeescode/Mp3Pirates/of/the/Code/Bean1/The/Cursed/Coin/JackSparrowTest.java
@@ -1,6 +1,7 @@
 package at.refugeescode.Mp3Pirates.of.the.Code.Bean1.The.Cursed.Coin;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

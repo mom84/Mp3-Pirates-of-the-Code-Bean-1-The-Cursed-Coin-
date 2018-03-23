fdaa7115fafbeda99e736734e394e83bd090b97c
unknown <engmohammmad1984@yahoo.com>
2018-03-23 11:55:52 +0100

    Added Spring Boot Files

---
+++ src/main/java/at/refugeescode/Mp3Pirates/of/the/Code/Bean1/The/Cursed/Coin/Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp3Pirates.of.the.Code.Bean1.The.Cursed.Coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
